690919d560dd2e3b4e92c40e204b24f12d53ab8b
aliasgharkarani <aliasgharkarani125@hotmail.com>
2019-01-25 18:40:58 +0500

    cleared previous

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';
+import Main from './main'
 import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+console.disableYellowBox=true;
+AppRegistry.registerComponent(appName, () => Main);
