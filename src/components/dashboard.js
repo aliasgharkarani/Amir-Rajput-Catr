a2db5166cb46ec2a8515609621045384bb455499
aliasgharkarani <aliasgharkarani125@hotmail.com>
2019-01-30 16:13:30 +0500

    Cart  Icon Fixed

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -2,9 +2,12 @@ import React, { Component, View } from 'react';
 import Screen from './screen'
 import {
     StyleSheet,
-    TouchableOpacity
+    TouchableOpacity,
+    Dimensions
 } from 'react-native';
 import { Container, Header, Content, Footer, FooterTab, Button, Icon, Text, Badge, Left, Body, Title, Right } from 'native-base';
+var height = Dimensions.get('window').height;
+var width = Dimensions.get("window").width;
 export default class FooterTabsBadgeExample extends Component {
     constructor(props) {
         super(props);
@@ -18,24 +21,16 @@ export default class FooterTabsBadgeExample extends Component {
         return (
             <Container contentContainerStyle={{ flex: 1 }}>
                 <Header style={styles.hdr}>
-                    {/* <Left> */}
-                    {/* <Button transparent> */}
-                    {/* <Icon name="arrow-back" /> */}
-                    {/* </Button> */}
-                    {/* </Left> */}
-                    {/* <Body style={{backgroundColor:"yellow"}}> */}
-                    <TouchableOpacity style={{ width: "20%" }}></TouchableOpacity>
-                    <TouchableOpacity style={{ width: "60%" }}>
+                    <TouchableOpacity activeOpacity={1} style={{ width: "20%" }}></TouchableOpacity>
+                    <TouchableOpacity activeOpacity={1} style={{ width: "60%",alignSelf: "center" }}>
                         <Text style={styles.heading}>{this.state.header}</Text>
                     </TouchableOpacity>
-                    {/* </Body> */}
-                    {/* <Right> */}
-                    <TouchableOpacity style={{ width: "20%",alignSelf:"center"}}>
-                        <Button transparent>
-                            <Icon name="search" />
+                    <TouchableOpacity activeOpacity={1} style={{ width: "20%", alignSelf: "center" }}>
+                        <Button style={{ elevation: 0, backgroundColor: "#C21807" }} badge vertical>
+                            <Badge style={{height:22,width:22,marginBottom:-7,zIndex:10}}><Text style={{fontSize:13,color:"white",height:20,width:22,alignSelf: "center"}}>{this.state.order}</Text></Badge>
+                            <Icon name="cart" />
                         </Button>
                     </TouchableOpacity>
-                    {/* </Right> */}
                 </Header>
                 <Content contentContainerStyle={{ flex: 1 }}>
                     <Screen ScrnChng={this.state.ScreenDisplay} />
@@ -57,7 +52,7 @@ export default class FooterTabsBadgeExample extends Component {
                             <Text>Pan Asian</Text>
                         </Button>
                         <Button style={{ elevation: 0 }} badge vertical onPress={() => { this.setState({ ScreenDisplay: 3, header: "PROFILE" }) }}>
-                            <Badge ><Text>{this.state.order}</Text></Badge>
+                            <Badge><Text>{this.state.order}</Text></Badge>
                             <Icon name="cart" />
                             <Text>PROFILE</Text>
                         </Button>
@@ -75,6 +70,7 @@ const styles = StyleSheet.create({
         color: 'white'
     },
     hdr: {
-        backgroundColor: '#C21807'
+        backgroundColor: '#C21807',
+        height:width/7.5
     }
 });
